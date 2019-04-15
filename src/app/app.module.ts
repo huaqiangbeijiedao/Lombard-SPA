aa740590c7e1c0d27b233c37f690f80de9b48605
Krystian Kijak <kry.kjjk@op.pl>
2019-04-15 15:58:37 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NavComponent } from './nav/nav.component';
-
 import { AddProductFormComponent } from './addProductForm/addProductForm.component';
 import { AddTransactionComponent } from './AddTransaction/AddTransaction.component';
 
@@ -19,8 +16,6 @@ import { AddTransactionComponent } from './AddTransaction/AddTransaction.compone
       AddProductFormComponent,
       NavComponent,
       AddTransactionComponent,
-      AddTransactionComponent,
-      AddTransactionComponent
    ],
    imports: [
       BrowserModule,
