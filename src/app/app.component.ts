028b927ea7f05660c833c5a1e3cdfa4c20cd0bfa
Arkko002 <arkko002@gmail.com>
2019-04-15 15:50:36 +0200

    eldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { ProductService } from 'src/services/ProductService';
+import { Product } from './Models/Product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Lombard-SPA';
+  Products : Observable<Product[]>
+  constructor(private api : ProductService){}
+
+  ngOnInit(){
+    this.Products = this.api.getAll();
+    console.log(this.Products);
+  }
 }
