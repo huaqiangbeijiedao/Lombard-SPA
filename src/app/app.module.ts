bb930e4e023bc81ecedb0cf8f61fa8aa01cf0985
Krystian Kijak <kry.kjjk@op.pl>
2019-04-15 14:19:40 +0200

    ad product Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './Product/Product.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
