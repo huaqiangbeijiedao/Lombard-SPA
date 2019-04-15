193a84ee816958a7e09ab688b2e79ecd26c24c35
Krystian Kijak <kry.kjjk@op.pl>
2019-04-15 15:50:03 +0200

    fix bugs

---
+++ src/app/AddTransaction/AddTransaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-AddTransaction',
+  templateUrl: './AddTransaction.component.html',
+  styleUrls: ['./AddTransaction.component.css']
+})
+export class AddTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NavComponent } from './nav/nav.component';
+
+import { AddProductFormComponent } from './addProductForm/addProductForm.component';
+import { AddTransactionComponent } from './AddTransaction/AddTransaction.component';
+import { AddTransactionComponent } from './AddTransaction/AddTransaction.component';
+import { AddTransactionComponent } from '.c:/Users/Krykjjk/Desktop/Lombard-View/Lombard-SPA/src/AddTransaction/AddTransaction.component';
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      AddProductFormComponent,
+      NavComponent,
+      AddTransactionComponent,
+      AddTransactionComponent,
+      AddTransactionComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
