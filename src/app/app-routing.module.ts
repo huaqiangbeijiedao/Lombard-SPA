1ee5540d53f5f3696d8d07a40161f60fcd75f0c2
Arkko002 <arkko002@gmail.com>
2019-04-16 11:13:21 +0200

    routes fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UpdateProductComponent } from "./updateProduct/updateProduct.component";
 import { ProductTableComponent } from "./productTable/productTable.component";
-import { AddTransactionComponent } from './AddTransaction/AddTransaction.component';
 import { TransactionTableComponent } from './TransactionTable/TransactionTable.component';
 
 const routes: Routes = [
   {path: "update", component:UpdateProductComponent },
   { path: "list", component: ProductTableComponent},
-  { path: "addtransation", component: AddTransactionComponent},
   { path: "showtransations", component: TransactionTableComponent},
 ];
 
