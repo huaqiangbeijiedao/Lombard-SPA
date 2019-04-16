d88de26b47bcc4ffd35dd027ba7df4d6d83ce4ca
Marcin Trocha <cinekmar21@gmail.com>
2019-04-16 12:52:21 +0200

    essa clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UpdateProductComponent } from "./updateProduct/updateProduct.component";
 import { ProductTableComponent } from "./productTable/productTable.component";
 import { OutageComponent } from "./Outage/Outage.component";
 import { TransactionTableComponent } from './TransactionTable/TransactionTable.component';
 
 const routes: Routes = [
-  {path: "update", component:UpdateProductComponent },
   { path: "list", component: ProductTableComponent},
   { path: "showtransations", component: TransactionTableComponent},
   {path: "outage", component: OutageComponent}
